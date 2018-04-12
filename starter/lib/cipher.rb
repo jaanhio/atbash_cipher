85c47edc5a265cc21380c0aa665462172de9e400
jaanhio <jianhaot.89@gmail.com>
2018-04-12 09:49:09 +0800

    decode and encode completed

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,16 +1,24 @@
 class Cipher 
   def initialize
-    @plain = ('a'..'z').to_a
+    @plain = ("a".."z").to_a
     @cipher = @plain.reverse
   end
 
   def encode(str)
     str.split("").map{|letter|
       letter = @cipher[@plain.index(letter)]
-    }
+  }.join("")
   end
+
+  def decode(str)
+    str.split("").map{|letter|
+      letter = @plain[@cipher.index(letter)]
+  }.join("")
+  end
+
 end
 
 encoder = Cipher.new()
 
 puts encoder.encode("hello")
+puts encoder.decode("svool")
