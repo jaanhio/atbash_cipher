b3609d1918b1a04e2d5d5696f3eed897d5c6e1db
jaanhio <jianhaot.89@gmail.com>
2018-04-12 09:46:44 +0800

    done

--- starter/lib/cipher.rb
+++ starter/lib/cipher.rb
@@ -1,2 +1,16 @@
-class Cipher
+class Cipher 
+  def initialize
+    @plain = ('a'..'z').to_a
+    @cipher = @plain.reverse
+  end
+
+  def encode(str)
+    str.split("").map{|letter|
+      letter = @cipher[@plain.index(letter)]
+    }
+  end
 end
+
+encoder = Cipher.new()
+
+puts encoder.encode("hello")
